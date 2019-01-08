9685d407647e23ad4d76dcab075ee8d8e228ef4f
magomedovgh@gmail.com <magomedovgh@gmail.com>
2019-01-08 16:54:58 +0300

    added new activity. learned to set event listeners and to launch new activities

---
+++ app/src/main/java/gm/taskscheduler/ui/LoginFormActivity.kt
@@ -0,0 +1,39 @@
+package gm.taskscheduler.ui
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.view.View
+import android.widget.EditText
+import android.widget.Toast
+import android.widget.Toast.makeText
+import gm.taskscheduler.R
+
+class LoginFormActivity : AppCompatActivity() {
+
+    private val LOGIN = "gjmrd"
+    private val PASSWORD = "admin"
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.login_form)
+    }
+
+    fun showDick(view: View) {
+        val loginField = findViewById<EditText>(R.id.editText2)
+        val passwordField = findViewById<EditText>(R.id.editText)
+
+        //showToast("test")
+
+        if ((loginField.value == LOGIN) && (passwordField.value == PASSWORD))
+            showToast("welcome to the club, buddy")
+        else
+            showToast("get your ass back, fucking slave")
+    }
+
+    var EditText.value: String
+        get() = this.text.toString()
+        set(value) = this.setText(value)
+
+   private fun showToast(text: String) = makeText(applicationContext, text, Toast.LENGTH_LONG).show()
+}
--- app/src/main/java/gm/taskscheduler/ui/MainActivity.kt
+++ app/src/main/java/gm/taskscheduler/ui/MainActivity.kt
@@ -1,9 +1,10 @@
-package gm.taskscheduler
+package gm.taskscheduler.ui
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
-import android.widget.Toast
+import gm.taskscheduler.R
 
 class MainActivity : AppCompatActivity() {
 
@@ -11,7 +12,11 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val button = findViewById<Button>(R.id.button)
-        button.setOnClickListener{Toast.makeText(applicationContext, "ААААААААААААААААААААААААААААААА", Toast.LENGTH_LONG).show()}
+        val intent = Intent(this, LoginFormActivity::class.java)
+        intent.putExtra("msg123", "1233")
+        button.setOnClickListener{startActivity(intent)}
 
     }
+
+    //fun showChlen(view: View) = Toast.makeText(applicationContext, "123", Toast.LENGTH_SHORT).show()
 }
